7da75f2abb205c3319241ca0d60885ce70bb4925
Augusto Julio <augusto.julio@ambar.tech>
2021-11-27 16:42:36 -0300

    Task 4 - retrieve UserId from CarOnSale RESTful API

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -2,18 +2,37 @@ import { inject, injectable } from "inversify";
 import { ILogger } from "./services/Logger/interface/ILogger";
 import { DependencyIdentifier } from "./DependencyIdentifiers";
 import "reflect-metadata";
+import express, { Application } from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
 
 @injectable()
 export class AuctionMonitorApp {
+  public constructor(
+    @inject(DependencyIdentifier.LOGGER) private logger: ILogger
+  ) {}
 
-    public constructor(
-        @inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
-    }
+  public async start(): Promise<void> {
+    this.logger.log(`Auction Monitor started.`);
 
-    public async start(): Promise<void> {
-        this.logger.log(`Auction Monitor started.`);
+    // TODO: Retrieve auctions and display aggregated information (see README.md)
+    app.get("/health", async (_req, res) => {
+      res.send({
+        message: "CarOnSale is ON",
+        date: new Date(),
+      });
+    });
+    app.get("/v1/authentication/:mailAddress/registered", async (_req, res) => {
+      res.sendStatus(204);
+      console.log(res);
+    });
+    // app.get("/v2/auction/seller/{userId}/running", async (_req, res)=>{
 
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
-    }
-
+    // })
+    app.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
+    });
+  }
 }
